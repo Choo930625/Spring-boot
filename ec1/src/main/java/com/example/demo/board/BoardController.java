1508f6a6c6bbfe291790490011830863709ba2b2
DESKTOP-AOV3NBB\QB_R&D <cis930625@gmail.com>
2020-09-24 08:56:31 +0900

    add ec1 folder

---
+++ ec1/src/main/java/com/example/demo/board/BoardController.java
@@ -0,0 +1,12 @@
+package com.example.demo.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+    @GetMapping("/test")
+    public String test() {
+        return "index.html";
+    }
+}
